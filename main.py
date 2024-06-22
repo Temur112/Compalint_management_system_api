8563ced8cc507aeed93c3bce8a7d29c56bb7c314
Umidov Temur <84757889+Temur112@users.noreply.github.com>
2024-06-22 15:48:24 +0500

    cors

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db import databse
 from resources.routers import api_router
 from contextlib import asynccontextmanager
+
+
+origins = ["http://localhost", "http://localhost:4200"]
 
 
 @asynccontextmanager
@@ -15,3 +19,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=True,
+    allow_crediential=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
